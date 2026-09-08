d277e402177119fc3407a94ad2b4ae9c98171bdb
zhanghaomin <zhang.haomin1106@gmail.com>
2026-09-08 10:47:27 +0800

    refactor(classical_chaotic_scattering): 复用 common/rk4_4d, 移除内联实现与死链接
    - main.c 删除与 common/rk4_4d 逐字相同的内联实现, 改包含共享头
    - Makefile 链接对象由未使用的 rk4_2d.c 换成实际需要的 rk4_4d.c
    - deriv 加 (void)t 消除未用参数警告; fopen 判空

--- project/c/classical_chaotic_scattering/main.c
+++ project/c/classical_chaotic_scattering/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#include "../common/rk4_2d.h"
+#include "../common/rk4_4d.h"
 
 #define h 1e-5
 #define y0 20
@@ -13,25 +13,8 @@ double t;
  *   (见笔记 13.3 节), 目前完全未实现 */
 /* TODO(P3): 积分区间 y=±20 与固定步长 h=1e-5 可接受, 但可考虑靠近势阱区用自适应步长 */
 
-typedef void (*deriv4)(double t, double y[4], double dydt[4]);
-
-void rk4_4d(deriv4 f, double y[4], double t, double dt) {
-    double k1[4], k2[4], k3[4], k4[4], yt[4];
-    int i;
-
-    f(t, y, k1);
-    for (i = 0; i < 4; i++) yt[i] = y[i] + 0.5 * dt * k1[i];
-    f(t + 0.5 * dt, yt, k2);
-    for (i = 0; i < 4; i++) yt[i] = y[i] + 0.5 * dt * k2[i];
-    f(t + 0.5 * dt, yt, k3);
-    for (i = 0; i < 4; i++) yt[i] = y[i] + dt * k3[i];
-    f(t + dt, yt, k4);
-
-    for (i = 0; i < 4; i++)
-        y[i] += dt / 6.0 * (k1[i] + 2.0 * k2[i] + 2.0 * k3[i] + k4[i]);
-}
-
 void deriv(double t,double *y,double *dydt){
+    (void)t; /*自治系统, 不显含时间*/
     dydt[0]=y[1];
     dydt[1]=-2*y[2]*y[2]*y[0]*(1-y[0]*y[0])*exp(-y[0]*y[0]-y[2]*y[2]);
     dydt[2]=y[3];
@@ -54,6 +37,10 @@ void init(double *p,double x,double xv,double y,double yv){
 int main(){
     double p[4];
     FILE *fp=fopen("b-theta.csv","w");
+    if(fp==NULL){
+        perror("fopen b-theta.csv");
+        return 1;
+    }
     for(int i=1;i<=1000;i++){
         double x0=(double)i/1000*2;
         init(p,x0,0,-y0,V0);
